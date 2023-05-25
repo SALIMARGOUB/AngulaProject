590a34ed04a5a80c2a82800797e2b380fb18fbb0
SALIMARGOUB <salimargoub@gmail.com>
2023-05-25 14:12:32 +0200

    rename-title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'nom-du-projet';
+  title = 'Bienvenue sur le site de Argoub Salim !';
 }
