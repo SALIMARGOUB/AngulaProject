3b0720bfc2ad715803cf91ceeb454d4469cb2a9d
SALIMARGOUB <salimargoub@gmail.com>
2023-05-26 14:10:18 +0200

    creation menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './sign-up/signup.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'sign-up', component: SignupComponent },
+  { path: 'user-profile', component: UserProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
@@ -7,19 +8,26 @@ import { AppComponent } from './app.component';
 import { ValidationButtonComponent } from './validation-button/validation-button.component';
 import { BlockComponentComponent } from './block-component/block-component.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignupComponent } from './sign-up/signup.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ValidationButtonComponent,
     BlockComponentComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    MenuComponent,
+    SignupComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
-],
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
